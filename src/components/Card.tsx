85bebbf562f31c2121a672a7bf684407989210bf
Sara-MF <sara.melo.dev@gmail.com>
2024-10-08 15:41:09 -0300

    style: ajustando identacao

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,13 @@
 type CardProps = {
-    className: string;
-    children: React.ReactNode;
-  };
-  
-  export default function Card({ className, children }: CardProps) {
-  
-    const style = className + " rounded-md";
-  
-    return (
-      <div className={style}>{children}</div>
-    )
-  }
+  className: string;
+  children: React.ReactNode;
+};
+
+export default function Card({ className, children }: CardProps) {
+
+  const style = className + " rounded-md";
+
+  return (
+    <div className={style}>{children}</div>
+  )
+}
